daf55e19e21e718d1bcfca6dceb104b60188fca8
Web Development Team <webdevlopemnet.cravity@gmail.com>
2025-09-19 11:02:22 +0000

    Initial commit — push Laravel project from public_html

---
+++ bootstrap/cache/biometric_module.php
@@ -0,0 +1,13 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Biometric\\Providers\\BiometricServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Biometric\\Providers\\BiometricServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
---
+++ bootstrap/cache/jobsheet_module.php
@@ -0,0 +1,15 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Modules\\Jobsheet\\Providers\\JobsheetServiceProvider',
+    1 => 'Modules\\Jobsheet\\Providers\\EventServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Modules\\Jobsheet\\Providers\\JobsheetServiceProvider',
+    1 => 'Modules\\Jobsheet\\Providers\\EventServiceProvider',
+  ),
+  'deferred' => 
+  array (
+  ),
+);
